b13bd9bae632218a78e63d2a0485dce1ba53f82d
WG67 <Withergaming67@gmail.com>
2023-04-18 08:33:58 +0100

    Revert "Added Git.sh which allows me to push commits to github"
    This reverts commit 94b1a8e3b4d6059d91b91dbdfb0dd5d046eb8d66.

--- git.sh
+++ git.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 git status
 echo "This is Your status:"
-git add .
+git add.
 echo "Please Enter Your Message:"
 read message
 git commit -m "$message"
-echo "Now Push to the main branch"
-git status
+
 
